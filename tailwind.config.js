e48c6b89eb1f9cdd152c72e639db68ce4e3b1768
attheodo <at@atworks.gr>
2026-02-01 17:29:51 +0200

    dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  darkMode: "class",
   content: [
     "./_includes/**/*.html",
     "./_layouts/**/*.html",
@@ -12,7 +13,8 @@ module.exports = {
         gray: {
           300: "#9F9F9F",
           400: "#616161",
-          500: "#1C0508" 
+          500: "#1C0508",
+          700: "#141414" 
         },
         brand: {
           500: "#E7374E"
